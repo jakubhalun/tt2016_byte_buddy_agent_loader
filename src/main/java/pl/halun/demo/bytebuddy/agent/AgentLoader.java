abe3d4b3a9e345b49fbca732c275170dcb5aad33
Jakub Hałun <jakubhal@gmail.com>
2016-10-25 07:57:59 +0200

    agent loader

---
+++ src/main/java/pl/halun/demo/bytebuddy/agent/AgentLoader.java
@@ -0,0 +1,17 @@
+package pl.halun.demo.bytebuddy.agent;
+
+import java.io.File;
+
+import net.bytebuddy.agent.ByteBuddyAgent;
+
+public class AgentLoader {
+	public static void main(String[] args) {
+		if (args.length != 2) {
+			System.err
+					.println("Please provide process id and path to agent jar");
+			System.exit(-1);
+		}
+
+		ByteBuddyAgent.attach(new File(args[1]), args[0]);
+	}
+}
